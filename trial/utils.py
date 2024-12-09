c164263d29caf10cde3a2da8c49f52f33ead9eae
Kidyoh <kidyoh789@gmail.com>
2024-12-09 19:45:33 +0300

    added new things

--- trial/utils.py
+++
@@ -1,7 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-def create_menu_keyboard(buttons):
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton(text, callback_data=data)]
-        for text, data in buttons
-    ])
